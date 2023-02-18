506633ca3008ec4017d6dde91bedc2203ead2fbf
Sahandgha <sahand.gha@gmail.com>
2023-02-18 09:02:40 +0700

    install tailwind css

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Google Clone Next js 13</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Google clone created by Next js 13" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" />
     </>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="text-red-600 text-3xl">Home</div>
     
   )
 }
