4751baa3c50507a3a13c83211298adddbc7cccc1
Sahandgha <sahand.gha@gmail.com>
2023-02-28 07:16:05 +0700

    remove the waiting Promise

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default async function ImageSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image&start=${startIndex}`
   );
