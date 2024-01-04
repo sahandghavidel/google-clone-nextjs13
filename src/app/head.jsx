13de89e5f1ce35142dc4d31d180d4c700f23471b
Sahandgha <sahand.gha@gmail.com>
2024-01-04 10:41:24 +0700

    update the head file

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Google Clone Next js 13</title>
+      <title>Google Clone Next js</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Google clone created by Next js 13" />
+      <meta name="description" content="Google clone created by Next js" />
       <link rel="icon" href="/favicon.svg" />
     </>
   )
